d62811ba974f1ca215253515fed3e58919488676
Alonso Brenes <alonso.brenes@gmail.com>
2025-12-04 09:35:22 -0600

    Dashboards by role

---
+++ Models/PatientsByPeriodDetailDto.cs
@@ -0,0 +1,28 @@
+﻿namespace EPApi.Models
+{
+    public sealed class PatientsByPeriodDetailDto
+    {
+        public DateTime Date { get; set; }
+        public Guid PatientId { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName1 { get; set; } = "";
+        public string? LastName2 { get; set; }
+        public string? IdentificationNumber { get; set; }
+        public int ContactsCount { get; set; }
+    }
+
+    public sealed class PatientsByPeriodBucketDto
+    {
+        public DateTime Date { get; set; }      // mismo bucket que arriba
+        public int PatientsCount { get; set; }  // pacientes únicos ese día
+    }
+
+    public sealed class PatientsByPeriodStatsDto
+    {
+        public int TotalUniquePatients { get; set; }
+        public int TotalContacts { get; set; }   // suma de ContactsCount
+
+        public List<PatientsByPeriodBucketDto> Series { get; set; } = new();
+        public List<PatientsByPeriodDetailDto> Details { get; set; } = new();
+    }
+}
